a1737d031e5cb2d6905b2e1047e4ccacca6e8d41
samantrs <https://samantrs@git.rabobank.nl>
2024-03-05 14:03:11 +0100

    integration test using test containers

---
+++ docker-containerization/job-service/src/test/java/com/spring/dockerize/jobservice/JobServiceIT.java
@@ -0,0 +1,59 @@
+package com.spring.dockerize.jobservice;
+
+import com.spring.dockerize.jobservice.dto.JobDto;
+import com.spring.dockerize.jobservice.generic.BaseTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Set;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class JobServiceIT extends BaseTest {
+
+	@Autowired
+	private WebTestClient client;
+
+	@Test
+	void getAllJobsTest() {
+		this.client.get()
+			.uri("/jobs/all")
+			.exchange()
+			.expectStatus().is2xxSuccessful()
+			.expectBody()
+			.consumeWith(e -> System.out.println(new String(e.getResponseBody())))
+			.jsonPath("$").isNotEmpty();
+	}
+
+	@Test
+	public void searchJobBySkillsTest() {
+		this.client.get()
+				.uri(uriBuilder -> uriBuilder
+						.path("/jobs/search")
+						.queryParam("skills", "project")
+						.build()
+				)
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody()
+				.consumeWith(e -> System.out.println(new String(e.getResponseBody())))
+				.jsonPath("$").isNotEmpty();
+	}
+
+	@Test
+	public void postJobTest() {
+		JobDto jobPayload = JobDto.create(null, "k8s engineer", "google", Set.of("k8s"), 200000, true);
+
+		this.client.post()
+				.uri("/jobs/")
+				.bodyValue(jobPayload)
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody()
+				.jsonPath("$.id").isNotEmpty()
+				.jsonPath("$.description").isEqualTo("k8s engineer");
+	}
+}
---
+++ docker-containerization/job-service/src/test/java/com/spring/dockerize/jobservice/generic/BaseTest.java
@@ -0,0 +1,31 @@
+package com.spring.dockerize.jobservice.generic;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+public abstract class BaseTest {
+
+    private static final int MONGO_PORT = 27017;
+    private static final String INIT_JS = "/docker-entrypoint-initdb.d/init.js";
+    private static final String MONGO_URI_FORMAT = "mongodb://root:rootpassword@%s:%s/job";
+
+    @Container
+    private static final GenericContainer mongoContainer = new GenericContainer(DockerImageName.parse("mongo"))
+            .withExposedPorts(MONGO_PORT)
+            .withClasspathResourceMapping("data/job-init.js", INIT_JS, BindMode.READ_ONLY)
+            .waitingFor(Wait.forListeningPort());
+
+    @DynamicPropertySource
+    static void mongoProperties(DynamicPropertyRegistry registry) {
+        mongoContainer.start();
+        String mongoUri = String.format(MONGO_URI_FORMAT, mongoContainer.getHost(), mongoContainer.getMappedPort(MONGO_PORT));
+        registry.add("spring.data.mongodb.uri", () -> mongoUri);
+    }
+}
